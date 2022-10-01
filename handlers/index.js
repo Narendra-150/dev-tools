56556ede4795bd279a6e30a263691c6add9c9c6c
Narendra <narendrabagul84@gmail.com>
2022-10-01 16:17:15 +0530

    initial commit - dummy api path added

--- handlers/index.js
+++ handlers/index.js
@@ -3,13 +3,16 @@
 const routes = require('express').Router({ mergeParams: true });
 
 module.exports = (services) => {
-    routes.use('/api/dates/getnumberofdays', require('./dates/getnumberofdays')(services));
-    routes.use('/api/filesystem/csvtodatabase', require('./filesystem/csvtodatabase')(services)); //to do
-    routes.use('/api/filesystem/jsontocsv', require('./filesystem/jsontocsv')(services)); //to do
+  //dates  
+  routes.use('/api/dates/getnumberofdays', require('./dates/getnumberofdays')(services));
 
-    routes.get('/', (req, res) => {
-        res.status(200).json({ message: 'Connected!' });
-      });
+  //filesystem
+  routes.use('/api/filesystem/csvtodatabase', require('./filesystem/csvtodatabase')(services)); //to do
+  routes.use('/api/filesystem/jsontocsv', require('./filesystem/jsontocsv')(services)); //to do
 
-    return routes;
+  routes.get('/', (req, res) => {
+    res.status(200).json({ message: 'Connected!' });
+  });
+
+  return routes;
 };
